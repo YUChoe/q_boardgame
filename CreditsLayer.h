4f1a8cc2c0ec70e0df737ec22fc37ea9b010c650
ls_pp <scr4free@gmail.com>
2012-02-08 17:07:21 +0900

    #24 Credits 레이어 작업 완료

--- CreditsLayer.h
+++ CreditsLayer.h
@@ -3,16 +3,20 @@
 //  QBoard
 //
 //  Created by Choe Yong-uk on 12. 2. 8..
-//  Copyright 2012년 __MyCompanyName__. All rights reserved.
+//  Copyright 2012년 noizze.net. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
 
 @interface CreditsLayer : CCLayer {
-    
+  UITextView *CreditsTextView;
+  UIButton *backButton;
 }
 
 +(CCScene *) scene;
+
+- (void) displayUI:(id)sender;
+- (void) backTouched:(id)sender;
 
 @end
--- CreditsLayer.m
+++ CreditsLayer.m
@@ -3,10 +3,11 @@
 //  QBoard
 //
 //  Created by Choe Yong-uk on 12. 2. 8..
-//  Copyright 2012년 __MyCompanyName__. All rights reserved.
+//  Copyright 2012년 noizze.net. All rights reserved.
 //
 
 #import "CreditsLayer.h"
+#import "menuLayer.h"
 
 
 @implementation CreditsLayer
@@ -30,10 +31,66 @@
 {
 	if( (self=[super init])) 
   {
-    self.isTouchEnabled = YES;
+    //self.isTouchEnabled = YES;
+    
+    CCSprite *bg = [CCSprite spriteWithFile:@"noizze_logo.png"];
+    bg.position = ccp(240, 160);
+    [self addChild:bg z:0];
+
+    id nextStep = [CCCallFuncN actionWithTarget:self selector:@selector(displayUI:)]; //  
+    
+    [self runAction: [CCSequence actions:[CCMoveBy actionWithDuration:0.5f position:ccp(0,0)], nextStep, nil]];
   }
   
   return self;
+}
+
+- (void) displayUI:(id)sender 
+{
+  
+  NSString *CreditsText =   @""
+  "* License \n"
+  "Copyright 2012 noizze.net, Yong-uk Choe \n"
+  "\n"
+  "* Bug tracker \n"
+  "have a bug or suggest? \n"
+  "https://github.com/YUChoe/q_boardgame/issues \n"
+  "\n"
+  "* Author \n"
+  "+ blog : http://blog.noizze.net \n"
+  "+ facebook group : http://www.facebook.com/groups/131873050221514/ \n"
+  "+ twitter : http://twitter.com/ls_pp \n"
+  "+ github : http://github.com/YUChoe \n";
+  
+  
+  CreditsTextView = [[UITextView alloc] initWithFrame:CGRectMake(20, 20, 260, 220)];
+  //[CreditsTextField setDelegate:self];
+  [CreditsTextView setFont:[UIFont fontWithName:@"Helvetica" size:12]];
+  CreditsTextView.backgroundColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.5f]; //[UIColor clearColor];
+  CreditsTextView.layer.cornerRadius = 5;
+  CreditsTextView.editable = NO;
+  
+  [CreditsTextView setText:CreditsText];
+  [[[CCDirector sharedDirector] openGLView] addSubview: CreditsTextView];
+  
+  backButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+  //[[UIButton alloc] initWithFrame:CGRectMake(280, 20, 60, 40)];
+  [backButton setFrame:CGRectMake(300, 20, 120, 40)];
+  [backButton setAlpha:0.75f];
+  [backButton setTitle:@"BACK" forState:UIControlStateNormal];
+  [backButton.titleLabel setTextAlignment:UITextAlignmentCenter];
+  
+  [backButton addTarget:self action:@selector(backTouched:) forControlEvents:UIControlEventTouchUpInside];
+  [[[CCDirector sharedDirector] openGLView] addSubview: backButton];
+}
+
+-(void)backTouched:(id)sender 
+{
+	//NSLog(@"Button was clicked");
+  [CreditsTextView removeFromSuperview];
+  [backButton removeFromSuperview];
+  
+  [[CCDirector sharedDirector] replaceScene:[CCTransitionFlipX transitionWithDuration:0.5f scene:[menuLayer scene]]];
 }
 
 // on "dealloc" you need to release all your retained objects
