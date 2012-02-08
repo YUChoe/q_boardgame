96c3f93fb11b3dc5cb9807260ccff09b42907a7e
ls_pp <scr4free@gmail.com>
2012-02-09 00:37:00 +0900

    #9 헤더파일 준비. CONFIG_FILE_NAME 정의

--- optionsLayer.h
+++ optionsLayer.h
@@ -3,16 +3,22 @@
 //  QBoard
 //
 //  Created by Choe Yong-uk on 12. 2. 8..
-//  Copyright 2012년 __MyCompanyName__. All rights reserved.
+//  Copyright 2012년 noizze.net. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
 
 @interface OptionsLayer : CCLayer {
-    
+  UIButton *backButton;
 }
 
 +(CCScene *) scene;
 
+- (void) displayUI:(id)sender;
+
+- (void) backTouched:(id)sender;
+
 @end
+
+#define CONFIG_FILE_NAME "config.xml"
